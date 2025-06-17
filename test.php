98f7328c4847884b1e51dc779e5965591976a061
Joylana <alanajrm@yahoo.com>
2025-06-17 14:56:09 -0500

    working copy function

--- test.php
+++ test.php
@@ -1,12 +1,6 @@
 <?php
-    //include('include/init.php');
-    date_default_timezone_set('America/Chicago');
-    $id = date("his");
-    echo $id;
+    include('include/init.php');
+    $move = createWorkoutFromProgram(814127);
+    debugOutput($move);
     
-    $add = rand(100000,889999);
-    $id = $id +$add;
-
-    echo "<br>generated id: ".$id;
-    echo "time: ". date("h:i:s");
 ?>
